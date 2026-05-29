27561c2a1eeb8da783d6265a3c179974f58bde2d
Chaluvaraj <chaluvashetty23@gmail.com>
2026-05-29 15:04:13 +0530

    updated

--- firstProject/src/App.jsx
+++ firstProject/src/App.jsx
@@ -1,12 +1,15 @@
 import React from 'react'
 import Example1 from './Module2/Example1'
 import Example2 from './Module2/Example2'
+import Example3 from './Module2/Example3'
+
 
 function App() {
   return (
     <div className="App">
       <Example1 />
       <Example2 />
+      <Example3 />
     </div>
   )
 }
---
+++ firstProject/src/Module2/Example3.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Example3() {
+  return (
+<>
+    <header>
+        <h1>welcome to react js class</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quasi culpa itaque quaerat fugit, similique sed? Dolore facilis praesentium maiores aliquam, sit laborum nisi ducimus tempore iure. Neque rem facilis eligendi?</p>
+      </header>
+      <section>
+        <h2>Section Title</h2>
+        <p>This is the content of the section. Lorem ipsum dolor sit amet consectetur adipisicing elit. Suscipit illo corrupti nihil labore eveniet vel architecto magnam nulla perferendis! Dolore cum unde perspiciatis eligendi quas numquam, non veritatis aut reprehenderit?</p>
+      </section>
+  </>
+)
+}
+export default Example3
