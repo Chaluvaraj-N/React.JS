c888dac4320dd3568eb2377435a22a5f79356545
Chaluvaraj <chaluvashetty23@gmail.com>
2026-07-07 11:28:12 +0530

    updated

--- firstProject/src/App.jsx
+++ firstProject/src/App.jsx
@@ -1,31 +1,9 @@
 import React from 'react'
-import ProtectedRoute from './Module11.1/ProtectedRoute'
-import Dashboard from './Module11.1/Dashboard'
-import Login from './Module11.1/Login'
-import Home from './Module11.1/Home'
-import Navbar from './Module11.1/Navbar'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import { useState } from 'react'
+import RegisterationForm from './Module13/RegisterationForm'
 function App() {
-  const [isAuth, setIsAuth] = useState(false)
   return (
-    <div >
-      <BrowserRouter>
- <Navbar/>
- <Routes>
-  <Route path="/" element={<Home/>}/>
-  <Route path="/login" element={<Login   setIsAuth={setIsAuth}/>}
-/>
-  {/* ProtectedRoute */}
-
- <Route path="/dashboard" element=
- {  <ProtectedRoute isAuth={isAuth}>
- <Dashboard  setIsAuth={setIsAuth}/>
-  </ProtectedRoute>
- }
-/>
- </Routes>
-      </BrowserRouter>
+    <div>
+      <RegisterationForm/>
     </div>
   )
 }
